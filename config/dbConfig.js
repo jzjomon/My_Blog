f73b84fb5cd74dddaf9192653a38cc9c581d149a
jomon jose <jzjomon@gmail.com>
2023-12-07 14:26:37 +0530

    some update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
       const conn = await mongoose.connect(process.env.MONGO_URL).then((result) => {
+        console.log(process.env.MONGO_URL);
         console.log(`MongoDB Connected`);
       }).catch((err) => {
         console.log("db not connected");
